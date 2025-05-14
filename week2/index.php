6ef3ee3f6e195e1a2950c1602ffe968da8603968
fenilghael <fenilghael28@gmail.com>
2025-05-14 16:37:03 -0400

    File Added

---
+++ week2/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<!-- Learning Echo -->
+    <?php
+
+      echo "Hello World!";
+
+      echo "<p> My name is Fenil Ghael 
+      </p>" 
+    ?>
+
+<!-- Learning variable -->
+
+    <?php
+    $fname = "Fenil";
+    $lname = "Ghael";
+    
+    echo "<h1>Hello " . $fname . " " . $lname . "</h1>";
+    
+    ?>
+
+</body>
+</html>
